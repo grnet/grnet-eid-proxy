e3615d7dfcd72dd10851cb36d36857d71f6e9589
newgrnetci <argo@grnet.gr>
2020-09-10 11:02:33 +0000

    Deploy website - based on 01ab42b41d979cb03bc10873f548dd01fada22aa

--- README.md
+++
@@ -1,1 +0,0 @@
-# GRNET eID Proxy
--- website/sidebars.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  someSidebar: {
-    About: ['about', 'high_level_architecture'],
-    Services : ['services', 'openid', 'saml_service_providers'],
-    References: ['references'],
-  },
-};
